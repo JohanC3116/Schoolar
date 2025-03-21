4f377f39b4f8298451b1892e5ee19003232a9a29
Johan <jccalderon.4697@unicesmag.edu.co>
2025-03-21 18:00:05 -0500

    Primer clase y primer conexion

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+
+    //config conection
+    $host = "localhost";
+    $port = "5432"; //puerto de la base de datos en postgres
+    $dbname = "schoolar";
+    $user = "postgres";
+    $password = "unicesmag";
+
+    //Create conction 
+    $conn = pg_connect("
+        host = $host
+        port = $port
+        dbname = $dbname
+        user = $user
+        password = $password
+    ");
+
+    if(!$conn){
+        die("Connection error: " . pg_last_error());
+    } else {
+        echo "Succes concetion";
+    }
+
+?>
