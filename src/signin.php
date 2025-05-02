ecd103f7fece8f710d1cffa3803aaeadba703df8
Johan <jccalderon.4697@unicesmag.edu.co>
2025-05-02 18:09:19 -0500

    Session in login

--- src/signin.php
+++ src/signin.php
@@ -1,27 +1,34 @@
 <?php
 
     include('../config/database.php');
+
+    session_start();
+    if(isset($_SESSION['user_id'])) {
+        header('Refresh:0; url=http://localhost/schoolar/src/home.php');
+    }
+
     $email = $_POST ['e_mail'];
     $passw = $_POST ['p_sswd'];
+    $enc_past = sha1($passw);
 
-
-    $sql = "select 
+    $sql = "select id,
 	COUNT(id) as total
 from 
 	users 
 where 
 	email = '$email' and 
-	password = '$passw' and 
+	password = '$enc_past' and 
 	status = true
-group by  
-	id;";
+    GROUP BY id";
 
     $res = pg_query($conn, $sql);
 
     if($res){
         $row = pg_fetch_assoc($res);
         if($row['total'] > 0){
-            echo "Login ok";
+            //echo "Login ok";
+            $_SESSION['user_id'] = $row['id'];
+            header('Refresh:0; url=http://localhost/schoolar/src/home.php');
         } else {
             echo "Login failed";
         }
