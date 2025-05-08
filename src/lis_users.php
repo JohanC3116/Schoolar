03774bd2ee67b866ce8dcec8a7ea5093f8143371
Johan <jccalderon.4697@unicesmag.edu.co>
2025-05-08 11:27:38 -0500

    Creation list_users

---
+++ src/lis_users.php
@@ -0,0 +1,38 @@
+<?php
+ include('../config/database.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" aling="center">
+        <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>E-mail</th>
+            <th>Status</th>
+            <th>.....   </th>
+        </tr>
+        <tr>
+            <th>Johan Camilo</th>
+            <th>Calderon Alvarez</th>
+            <th>jccalderon.4697@unicesmag.edu.co</th>
+            <th>Active  </th>
+            <th>
+                <img src="icons/edit/curriculum.png" width="40">
+                <img src="icons/edit/images (1).png" width="40">
+                <img src="icons/edit/2622200.png" width="40">
+            </th>
+        </tr>
+        <?php
+            //codigo
+           $sql = "";
+        ?>
+    </table>
+</body>
+</html>
